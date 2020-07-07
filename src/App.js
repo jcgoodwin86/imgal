6ee10718efa8a621535f9106b92106b397b5e3cc
jcgoodwin86 <jcgoodwin86@users.noreply.github.com>
2020-07-07 10:32:20 -0400

    moving to other comp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react';
+import React, {
+  useState,
+  useEffect,
+  useCallback,
+  useRef,
+  useReducer,
+} from 'react';
 import './App.css';
 import snoowrap from 'snoowrap';
 import { checkURL, fetchAnonymousToken } from './helpers';
 import Header from './components/Header';
-import Macy from 'macy'
+import Macy from 'macy';
 
 function App() {
-
   window.setTimeout(() => {
     Macy.init({
       container: '.content',
@@ -18,19 +23,43 @@ function App() {
         1200: 6,
         940: 3,
         520: 2,
-        400: 1
-      }
-    })
+        400: 1,
+      },
+    });
     return true;
   }, 0);
 
-  const [links, setLinks] = useState(null);
+  const imgReducer = (state, action) => {
+    switch (action.type) {
+      case 'STACK_IMAGES':
+        return { ...state, images: state.images.concat(action.images) };
+      case 'FETCHING_IMAGES':
+        return { ...state, fetching: action.fetching };
+      default:
+        return state;
+    }
+  };
+
+  const pageReducer = (state, action) => {
+    switch (action.type) {
+      case 'ADVANCE_PAGE':
+        return { ...state, after: state.after + 25 };
+      default:
+        return state;
+    }
+  };
+
+  const [pager, pagerDispatch] = useReducer(pageReducer, { after: 0 });
+
+  const [imgData, imgDispatch] = useReducer(imgReducer, {
+    images: [],
+    fetching: true,
+  });
+
   const [sub, setSub] = useState('husky');
-  // const [anonymousSnoowrap, setAnonymousSnoowrap] = useState(null)
   let anonymousSnoowrap = useRef(null);
 
-
-  const setSnoowrap = useCallback( async () => {
+  const setSnoowrap = useCallback(async () => {
     const anonymousToken = localStorage.getItem('anonymousToken');
     const time = localStorage.getItem('time');
     const currentTime = Math.round(new Date().getTime() / 1000.0); // Time in epoch
@@ -48,41 +77,49 @@ function App() {
     anonymousSnoowrap.current = new snoowrap({
       userAgent: 'imgal',
       accessToken: token || anonymousToken,
-    })
-  }, [])
-
-  const getPosts = useCallback( async () => {
-    setLinks(await anonymousSnoowrap.current
-      .getHot(sub)
-      .reduce((newList, post, key) => {
-        if (checkURL(post.url)) {
-          newList.push(<img src={post.url} key={key} alt="test" />);
-        }
-        return newList;
-      }, []));
-  }, [anonymousSnoowrap, sub])
+    });
+  }, []);
 
   // Function for user to change subreddit
-  const setSubreddit = newSubreddit => {
-    setSub(newSubreddit)
+  const setSubreddit = (newSubreddit) => {
+    setSub(newSubreddit);
   };
 
-  useEffect(() =>{
+  useEffect(() => {
     async function startUp() {
       await setSnoowrap();
-      getPosts();
+      // getPosts();
+      imgDispatch({ type: 'FETCHING_IMAGES', fetching: true });
+      imgDispatch({
+        type: 'STACK_IMAGES',
+        images: await anonymousSnoowrap.current.getHot(sub, {
+          limit: 25,
+          after: pager.after,
+        }),
+      });
     }
     startUp();
-  },[getPosts, setSnoowrap, sub])
+  }, [setSnoowrap, sub]);
 
   return (
     <div className="App">
-      <Header setSubreddit={setSubreddit} currentSubreddit={sub}/>
-      <div className='content'>
-        {links}
+      <Header setSubreddit={setSubreddit} currentSubreddit={sub} />
+      <div className="content">
+        {imgData.images.map((post, i) => {
+          if (checkURL(post.url)) {
+            return (
+              <img
+                src={post.url}
+                key={i}
+                alt={post.title}
+                className="card-img-top"
+              />
+            );
+          }
+        })}
       </div>
     </div>
-      );
+  );
 }
 
 export default App;
